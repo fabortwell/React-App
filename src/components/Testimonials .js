05962249791423904077adb72062db098f063d03
Brian Sitati <briansit93@gmail.com>
2022-04-29 22:47:27 +0300

    added some styling to the testimonials

--- src/components/Testimonials .js
+++ src/components/Testimonials .js
@@ -9,8 +9,35 @@ const Testimonials  = () => {
   return (
     <div className='testimonials'>
         <div className='container'>
+        <h2> Testimonials</h2>
+        <span className='line'></span>
+        <div className='content'>
+            <div className='card'>
+           <img src={user1} alt='user1' />
+           <p>It is not every day that you come across a passionate and trustworthy software advisor. Brian Sitati is true professional who does his work really well. Thanks Brian!</p>
+           <p><span> Johnson.M.J.</span></p>
+           <p>Director of "Financial Times"</p>
+            </div>
+
+            <div className='card'>
+           <img src={user2} alt='user2' />
+           <p> In just 2 very short meetings with John he managed to find the solution personally catered to my situation and expectations. Punctual, well informed and very reliable.In just 2 very short meetings with John he managed to find the solution personally catered to my situation and expectations. Punctual, well informed and very reliable.</p>
+           <p>Carol Harper<span></span></p>
+           <p>Director at Risktec Solutions Ltd</p>
+            </div>
 
 
+            <div className='card'>
+           <img src={user3} alt='user3' />
+           <p> A very professional financial advisor, who is true to his word. John has demonstrated a high amount of integrity in the time I have known him, and he is fast to respond to my concerns.</p>
+           <p><span>Snow.J.R.</span></p>
+           <p>Managing Director of BPW Global</p>
+            </div>
+
+
+
+
+            </div>
         </div>
     </div>
   )
