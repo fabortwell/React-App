6f2e302128e6ffd6ca32e4fca18bd7c61f7f9340
Brian Sitati <briansit93@gmail.com>
2022-04-29 22:16:06 +0300

    added testimonial component

---
+++ src/components/Testimonials .js
@@ -0,0 +1,19 @@
+import React from 'react'
+import user1 from './images/user1.jpeg'
+import user2 from './images/user2.jpeg'
+import user3 from './images/user3.jpeg'
+import './Testimonials.css'
+
+
+const Testimonials  = () => {
+  return (
+    <div className='testimonials'>
+        <div className='container'>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials 
