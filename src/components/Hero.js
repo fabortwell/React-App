ea09e2ad5591ca98f027cadbfd7b561dd3fb3e9d
Brian Sitati <briansit93@gmail.com>
2022-04-29 20:02:43 +0300

    added a hero image and styled it in css

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+    <div className='content'>
+        <p>Call me</p>
+        <p>1-800-123-4567</p>
+        <p>Because Money</p>
+        <p>Does'nt come with instructions</p>
+        <button href='/' className='button'> Free consultation</button>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
